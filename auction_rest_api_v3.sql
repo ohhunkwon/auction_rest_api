b14dbf696106d289d76fd0a176ec2a68749bed26
Ohhun Kwon <ohhunkwon@hotmail.com>
2021-10-30 17:46:27 +0100

    Deleted redundant sql files and read method of Watchlist implemented

--- auction_rest_api_v3.sql
+++ auction_rest_api_v3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 25, 2021 at 08:55 AM
+-- Generation Time: Oct 30, 2021 at 04:42 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -43,7 +43,8 @@ INSERT INTO `Bids` (`bidID`, `createdAt`, `amount`, `userID`, `itemID`) VALUES
 (1, '2021-10-22 08:02:18', '770', 3, 11),
 (2, '2021-10-22 08:04:45', '68', 1, 12),
 (3, '2021-10-22 08:04:45', '800', 3, 13),
-(4, '2021-10-22 08:15:49', '1000', 4, 13);
+(4, '2021-10-22 08:15:49', '1000', 4, 14),
+(5, '2021-10-30 17:38:32', '1250', 1, 16);
 
 -- --------------------------------------------------------
 
@@ -70,11 +71,11 @@ CREATE TABLE `Items` (
 --
 
 INSERT INTO `Items` (`itemID`, `title`, `description`, `category`, `startingPrice`, `reservePrice`, `startDateTime`, `endDateTime`, `image`, `userID`, `bidID`) VALUES
-(11, '42 inch TV', 'adasdfewfwe', 'Electronics', '400', '500', '2021-10-22 07:01:07', '2021-10-24 08:01:08', 'image.png', 1, NULL),
-(12, 'Parka Coat', 'savasd sdf dsa fs s', 'Fashion', '50', '60', '2021-10-22 07:02:29', '2021-10-28 08:02:30', 'image.png', 2, NULL),
-(13, 'Macbook Pro 2018', 'adsafvs da sa', 'Electronics', '700', '850', '2021-10-22 07:02:29', '2021-10-29 08:02:30', 'image.jpeg', 4, NULL),
-(14, 'Tennis Racket', 'asdfnnja jkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 2, NULL),
-(16, 'Rare £1 Coin', 'adsadssda', 'Collectibles', '300', '450', '2021-10-25 10:00:00', '2021-11-10 10:00:00', 'image.jpg', 1, NULL);
+(11, '42 inch TV', 'adasdfewfwe', 'Electronics', '400', '500', '2021-10-22 07:01:07', '2021-10-24 08:01:08', 'image.png', 1, 1),
+(12, 'Parka Coat', 'savasd sdf dsa fs s', 'Fashion', '50', '60', '2021-10-22 07:02:29', '2021-10-28 08:02:30', 'image.png', 2, 2),
+(13, 'Macbook Pro 2018', 'adsafvs da sa', 'Electronics', '700', '850', '2021-10-22 07:02:29', '2021-10-29 08:02:30', 'image.jpeg', 4, 3),
+(14, 'Tennis Racket', 'asdfnnja jkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 2, 4),
+(16, 'Rare £1 Coin', 'adsadssda', 'Collectibles', '300', '450', '2021-10-25 10:00:00', '2021-11-10 10:00:00', 'image.jpg', 1, 5);
 
 -- --------------------------------------------------------
 
@@ -103,6 +104,28 @@ INSERT INTO `Users` (`userID`, `firstName`, `lastName`, `email`, `role`, `create
 (4, 'David', 'Smith', 'DavidSmith1991@gmail.com', 'Buyer', '2021-10-22 07:47:02', '2021-10-22 06:44:34'),
 (5, 'Alex', 'Smith', 'alexsmith@gmail.com', 'Buyer', '2021-10-22 07:47:02', '2021-10-22 06:44:34');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Watchlist`
+--
+
+CREATE TABLE `Watchlist` (
+  `userID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Watchlist`
+--
+
+INSERT INTO `Watchlist` (`userID`, `itemID`) VALUES
+(5, 13),
+(4, 16),
+(2, 12),
+(1, 13),
+(5, 11);
+
 --
 -- Indexes for dumped tables
 --
@@ -130,6 +153,13 @@ ALTER TABLE `Users`
   ADD PRIMARY KEY (`userID`);
 
 --
+-- Indexes for table `Watchlist`
+--
+ALTER TABLE `Watchlist`
+  ADD KEY `itemID` (`itemID`),
+  ADD KEY `userID` (`userID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -137,7 +167,7 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `Bids`
 --
 ALTER TABLE `Bids`
-  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `Items`
@@ -168,6 +198,13 @@ ALTER TABLE `Bids`
 ALTER TABLE `Items`
   ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`bidID`) REFERENCES `Bids` (`bidID`),
   ADD CONSTRAINT `items_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`);
+
+--
+-- Constraints for table `Watchlist`
+--
+ALTER TABLE `Watchlist`
+  ADD CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `Items` (`itemID`),
+  ADD CONSTRAINT `watchlist_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
