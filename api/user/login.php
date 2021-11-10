f05a59edc9d199c7df72a1181260c198fd9d6653
Vivek Vijay <vivekvijay123@gmail.com>
2021-11-10 16:37:54 +0000

    Updated login.php

--- api/user/login.php
+++ api/user/login.php
@@ -17,30 +17,38 @@
     $user = new User($db);
 
     // Validation of data entry
-    $data = $_POST;
+    //$data = $_POST;
+    $data = json_decode(file_get_contents("php://input"));
 
-    if (empty($data['username']) ||
-        empty($data['pw']) || {
-        echo 'Please fill in all required fields!';
+    if (empty($data['userID']) ||
+        empty($data['pwhash'])) {
+        echo json_encode(
+                array('message' => 'Please fill in all required fields!') 
+        );
         // Redirect to login page (is this necessary/is there a better way?)
         $loginURL = 'http://' . $_SERVER['HTTP_HOST'] .
         dirname($_SERVER['PHP_SELF']) . '/login.php';
         header('Location: ' . $loginURL);
         }
 
+    $user->userID = $data->userID;
+    $user->pwhash = $data->pwhash;
+    
     // Verify login, start session and redirect to index.php
 
-    $query = "SELECT userID, username FROM User " . "WHERE user.username = '$username' AND " . "pw = SHA('$pw')";
+    $query = "SELECT userID FROM User " . "WHERE user.userID = '$userID' AND " . "user.pwhash = SHA('$pwhash')";
     $data = mysqli_query($conn, $query);
     if (mysqli_num_rows($data) == 1) {
         $row = mysqli_fetch_array($data);
         $_SESSION['userID'] = $row['userID'];
-        $_SESSION['username'] = $row['username'];
+        //$_SESSION['username'] = $row['username'];
         $indexURL = 'http://' . $_SERVER['HTTP_HOST'] .
         dirname($_SERVER['PHP_SELF']) . '/index.php';
         header('Location: ' . $indexURL);
     } else {
-        echo 'Invalid email or password.';
+        echo json_encode(
+            array('message' =>'Invalid email or password.')
+         );
         // Redirect to login page
         $loginURL = 'http://' . $_SERVER['HTTP_HOST'] .
         dirname($_SERVER['PHP_SELF']) . '/login.php';
