116981fc0eb518c59a7c0538fe32c0578a62e4b4
ryzzaki <cuong.nugi@gmail.com>
2021-11-27 13:52:03 +0000

    fix: test options preflight

--- api/bids/create.php
+++ api/bids/create.php
@@ -5,6 +5,11 @@
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        // The request is using the POST method
+        header("HTTP/1.1 200 OK");
+        return;
+    }
     include_once '../../config/Database.php';
     include_once '../../models/Bids.php';
 
