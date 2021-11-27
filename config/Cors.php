bdb385ce38a1606db6274e533a9ac84210475fd0
ryzzaki <cuong.nugi@gmail.com>
2021-11-27 13:46:01 +0000

    feat: add options

--- config/Cors.php
+++ config/Cors.php
@@ -3,6 +3,6 @@ $origin = getenv('ORIGIN_URL');
 header("Access-Control-Allow-Origin: {$origin}");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST, PUT');
+header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 ?>
