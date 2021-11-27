edbfff9a72235130bb78c8c73601219c0b956c9e
ryzzaki <cuong.nugi@gmail.com>
2021-11-27 03:52:20 +0000

    feat: add lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0d9a445d4a573c6289162779e13e0dd0272453e5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0d9a445d4a573c6289162779e13e0dd0272453e5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
