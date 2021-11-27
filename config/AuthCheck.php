13e54a5e31e292d7fce1a24df8df380780553e59
ryzzaki <cuong.nugi@gmail.com>
2021-11-27 02:23:14 +0000

    fix: add cors to auth check

--- config/AuthCheck.php
+++ config/AuthCheck.php
@@ -1,8 +1,8 @@
 <?php
+    include('../../config/Cors.php');
 
-if(!isset($_COOKIE["PHPSESSID"])) {
-    http_response_code(401);
-    die();
-}
-
+    if(!isset($_COOKIE["PHPSESSID"])) {
+        http_response_code(401);
+        die();
+    }
 ?>
