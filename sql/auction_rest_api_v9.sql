c22d6c5de1bdbabefa038589cfc2b0628c686d7a
Ohhun Kwon <ohhunkwon@hotmail.com>
2021-11-30 18:44:25 +0000

    fix: added active default enum value for auctionStatus

--- sql/auction_rest_api_v9.sql
+++ sql/auction_rest_api_v9.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 30, 2021 at 03:27 PM
+-- Generation Time: Nov 30, 2021 at 06:41 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -90,7 +90,7 @@ CREATE TABLE `Items` (
   `userID` int(11) NOT NULL,
   `bidID` int(11) DEFAULT NULL,
   `highestPrice` int(11) DEFAULT NULL,
-  `auctionStatus` enum('active','inactive') DEFAULT NULL,
+  `auctionStatus` enum('active','inactive') DEFAULT 'active',
   `winner` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -99,15 +99,15 @@ CREATE TABLE `Items` (
 --
 
 INSERT INTO `Items` (`itemID`, `title`, `description`, `category`, `startingPrice`, `reservePrice`, `startDateTime`, `endDateTime`, `image`, `userID`, `bidID`, `highestPrice`, `auctionStatus`, `winner`) VALUES
-(11, '42 inch TV', 'adasdfewfwe', 'Electronics', '400', '500', '2021-10-22 07:01:07', '2021-10-24 08:01:08', 'image.png', 1, 1, 500, 'active', NULL),
-(12, 'Parka Coat', 'savasd sdf dsa fs s', 'Fashion', '50', '60', '2021-10-22 07:02:29', '2021-10-28 08:02:30', 'image.png', 2, 22, 1505, 'active', NULL),
+(11, '42 inch TV', 'adasdfewfwe', 'Electronics', '400', '500', '2021-10-22 07:01:07', '2021-10-24 08:01:08', 'image.png', 1, 1, 500, 'inactive', 3),
+(12, 'Parka Coat', 'savasd sdf dsa fs s', 'Fashion', '50', '60', '2021-10-22 07:02:29', '2021-10-28 08:02:30', 'image.png', 2, 22, 1505, 'inactive', 3),
 (13, 'Macbook Pro 2018', 'adsafvs da sa', 'Electronics', '700', '850', '2021-10-22 07:02:29', '2021-10-29 08:02:30', 'image.jpeg', 4, 23, 1600, 'inactive', NULL),
 (14, 'Tennis Racket', 'asdfnnja jkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 2, 3, 1100, 'inactive', NULL),
 (16, 'Rare £1 Coin', 'adsadssda', 'Collectibles', '300', '450', '2021-10-25 10:00:00', '2021-11-10 10:00:00', 'image.jpg', 1, 5, 1000, NULL, NULL),
 (17, 'Nike shoes', 'asdfnsdasdaa adjkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 3, 28, 40, NULL, NULL),
 (18, 'Some Item 1.0', 'asdfnsdasdaa adjkva vv', 'Motors', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 15, 27, 30, NULL, NULL),
 (19, 'Some Item 2.0', 'asdfnsdasdaa adjkva vv', 'Electronics', '25', '30', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 15, 26, 60, NULL, 16),
-(20, 'This is going to be bidded', 'asdfnsdasdaa adjkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 15, 30, 200, 'inactive', 16),
+(20, 'This is going to be bidded', 'asdfnsdasdaa adjkva vv', 'Sports', '15', '20', '2021-12-23 10:00:00', '2021-12-29 08:00:30', 'image.jpg', 15, 30, 200, 'inactive', 16),
 (21, 'This is going to be bidded', 'asdfnsdasdaa adjkva vv', 'Sports', '15', '20', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 15, NULL, 20, 'inactive', 3),
 (22, 'Adidas shoes', 'asdfnsdasdaa adjkva vv', 'Sports', '25', '50', '2021-10-23 10:00:00', '2021-10-29 08:00:30', 'image.jpg', 15, NULL, NULL, NULL, 16);
 
